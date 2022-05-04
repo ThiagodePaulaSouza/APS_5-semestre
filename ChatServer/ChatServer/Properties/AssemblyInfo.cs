05aeb96e986f398a5f988673e432b4c33a746131
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-05-04 00:43:24 -0300

    add ChatServer || Organizing Directories

--- ChatServer/ChatServer/Properties/AssemblyInfo.cs
+++ ChatServer/ChatServer/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("prjServer")]
+[assembly: AssemblyTitle("ChatServer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("prjServer")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyProduct("ChatServer")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1bd75888-acad-4fc5-be65-77c9a03e1774")]
+[assembly: Guid("6d7e7903-3067-426d-9857-be9bd8fa1da5")]
 
 // Version information for an assembly consists of the following four values:
 //
