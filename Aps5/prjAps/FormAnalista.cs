6c5a7a6dcc104fc498931656789cdd04b69fa93c
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-05-06 02:34:20 -0300

    fix - need to fill all the infos to get an access to analista or denunciante form
    fix - analista form can connect with server and when application exit, the connection finish, and application exit too.
    
    bug - when open analistaform with chatserver's running appers a messageBox with an error...

--- Aps5/prjAps/FormAnalista.cs
+++ Aps5/prjAps/FormAnalista.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace prjAps
 {
@@ -15,15 +18,42 @@ namespace prjAps
     {
         private string NameAnalista, LastnameAnalista;
 
+        private StreamWriter StwEnvia;
+        private StreamReader StrRecebe;
+        private TcpClient TcpServidor;
+
+        private IPAddress EndIP = IPAddress.Parse("127.0.0.1");
+        private int PortaHost = 1000;
+
+        //atualiza formulario com a mensagem de outra thread
+        private delegate void AtualizaLogCallBack(string strMensagem);
+        //Define o formulario para o estado "disconnected" de outra thread
+        private delegate void FechaConexaoCallBack(string strMotivo);
+
+        private bool Conectado;
+        private Thread MsgThread;
+
         public FormAnalista(string name, string lastname, string state, string city, int typeUser)
         {
             this.NameAnalista = name;
             this.LastnameAnalista = lastname;
 
+            Application.ApplicationExit += new EventHandler(OnApplicationExit);
+
             InitializeComponent();
+
             txt_nome.Text = $"{name} {lastname}";
             txt_estado.Text = state;
             txt_cidade.Text = city;
+
+            if (!Conectado)
+            {
+                InicializaConexao();
+            }
+            else
+            {
+                FechaConexao("Desconectado a pedido do analista");
+            }
 
             IPAddress[] ip = Dns.GetHostAddresses(Dns.GetHostName());
             txt_IP.Text = $"{ip[1].ToString()}";
@@ -35,9 +65,6 @@ namespace prjAps
             {
                 txt_ID.Text = $"{name[0]}IDP{lastname[0]}{typeUser}{ip[1]}";
             }
-
-            //conectar no server
-            //atualizar log do atendimento
         }
 
         #region propriedades
@@ -48,19 +75,142 @@ namespace prjAps
         }
 
         #endregion
+        
+        public void OnApplicationExit(object sender, EventArgs e)
+        {
+            if (Conectado)
+            {
+                // Fecha as conexões, streams, etc...
+                Conectado = false;
+                StwEnvia.Close();
+                StrRecebe.Close();
+                TcpServidor.Close();
+                
+            }
+        }
+
+        private void InicializaConexao()
+        {
+            try
+            {
+                TcpServidor = new TcpClient();
+                TcpServidor.Connect(EndIP, PortaHost);
+                Conectado = true;
+
+                //inicializa componentes
+                //txtMensagem.Enabled = true;
+                //btnEnviar.Enabled = true;
+
+                //envia o nome do user para o servidor
+                StwEnvia = new StreamWriter(TcpServidor.GetStream());
+                StwEnvia.WriteLine($"{NameAnalista} {LastnameAnalista}");
+                StwEnvia.Flush();
+
+                MsgThread = new Thread(new ThreadStart(RecebeMensagem));
+                MsgThread.IsBackground = true;
+                MsgThread.Start();
+            }
+            catch (Exception ex)
+            {
+                FechaConexao("Não foi possivel estabelecer conexão com o servidor...");
+                return;
+            }
+        }
+
+        private void AtualizaLog(string strMensagem)
+        {
+            atendimentoLog.AppendText($"{strMensagem} \r\n");
+        }
+
+        private void RecebeMensagem()
+        {
+            StrRecebe = new StreamReader(TcpServidor.GetStream());
+            string ConResposta = StrRecebe.ReadLine();
+
+            //ta dando erro aqui...
+            //o erro é: não é possivel chamar invoke 
+            //em um controle antes da criação do identificador de janela
+            try
+            {
+                if (ConResposta[0] == '1')
+                {
+                    //atualiza o formulário (do servidor) para informar que está conectado 
+                    this.Invoke(new AtualizaLogCallBack(this.AtualizaLog), new object[] { "Conectado com sucesso!" });
+                }
+                else
+                {
+                    string Motivo = "Não Conectado: ";
+
+                    //extrai o motivo da mensagem resposta o motivo começa no 3char
+                    Motivo += ConResposta.Substring(2, ConResposta.Length - 2);
+
+                    //atualiza o formulário (do servidor) para informar que está conectado 
+
+                    //Varificar oq muda...
+                    this.Invoke(new FechaConexaoCallBack(this.AtualizaLog), new object[] { Motivo });
+                    return;
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("o problema é \r\n" + ex);
+            }
+            while (Conectado)
+            {
+                //ERRO 
+                //provavelmente o erro n é aqui, mas sim no try catch acima
+
+                // não é possivel chamar invoke em um controle antes da criação do identificador de janela
+
+                //exibe mensagem no txtLog
+                this.Invoke(new AtualizaLogCallBack(this.AtualizaLog), new object[] { StrRecebe.ReadLine() });
+
+                //ERRO 
+            }
+        }
+        
+        private void FechaConexao(string Motivo)
+        {
+            //mostra motivo do porque fechou
+            atendimentoLog.AppendText($"{Motivo} \r\n");
+            //desabilita e abilita os campos apropriados
+            //txtMensagem.Enabled = false;
+            //btnEnviar.Enabled = false;
+
+            //fecha os objetos
+            Conectado = false;
+            TcpServidor.Close();
+            try
+            {
+                StwEnvia.Close();
+                StrRecebe.Close();
+            }
+            catch
+            {
+                //mostra motivo do porque fechou
+                atendimentoLog.AppendText($"{Motivo} \r\n");
+            }
+        }
+        
+        private void FormAnalista_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+        
 
         private void link_chat_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             //chamar chat
-            if (Application.OpenForms.OfType<FormChat>().Count() > 0 )
-            {
-                MessageBox.Show("O ATENDIMENTO JÁ ESTÁ SENDO REALIZADO!");
-            }
-            else
-            {
-                FormChat frmChat = new FormChat(NameAnalista, LastnameAnalista);
-                frmChat.Show();
-            }
-        }
+            //if (Application.OpenForms.OfType<FormChat>().Count() > 0)
+            //{
+            //    MessageBox.Show("O ATENDIMENTO JÁ ESTÁ SENDO REALIZADO!");
+            //}
+            //else
+            //{
+            //    FormChat frmChat = new FormChat(NameAnalista, LastnameAnalista);
+            //    frmChat.ShowDialog();
+            //}
+    }
     }
 }
