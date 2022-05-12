0ee54ac777b5d55468d7a16fd9af4357764344ee
HenriqueAlonsoDev <henrique.alonso@qindin.com.br>
2022-05-12 03:09:03 -0300

    try catch em todos os invokes - Inibe uma chamada de invoke enquanto as dependencias do form e o identificador de janela não foram iniciados

--- Aps5/prjAps/FormChat.Designer.cs
+++ Aps5/prjAps/FormChat.Designer.cs
@@ -139,6 +139,7 @@
             this.Name = "FormChat";
             this.Text = "Chat";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormChat_FormClosed);
+            this.Load += new System.EventHandler(this.FormChat_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
